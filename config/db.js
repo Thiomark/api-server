da07819a99d4c525b5392393741452dc27904755
itumeleng <thiotsoela@gmail.com>
2023-02-07 23:07:11 +0200

    cleaning code

--- config/db.js
+++ config/db.js
@@ -17,3 +17,13 @@ const connectDB = async () => {
 }
 
 module.exports = connectDB
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://thiomark:<password>@cluster0.70mkd0l.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
--- controllers/productController.js
+++ controllers/productController.js
@@ -8,7 +8,6 @@ const getProducts = asyncHandler(async (req, res) => {
     const pageSize = 12
     const page = Number(req.query.pageNumber) || 1
 
-
     const keyword = req.query.keyword
         ? {
             name: {
